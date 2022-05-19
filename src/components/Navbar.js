803f3eb7d297c37330f905e2fbb3f661304cc5e5
ZhangXiang <zhangxiang1121@gmail.com>
2022-05-19 13:40:35 +0800

    start react practice

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+//rfce :short cut
+
+
+import React, {useState} from 'react'
+
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-content'>
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
